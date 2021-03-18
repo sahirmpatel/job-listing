d23d2298628ca02aad8c38e6b0c8d5097c0ce429
Sahir Patel <sahirmpatel@gmail.com>
2021-03-18 14:04:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  color: pink;
+  border-radius: 5px;
+  background-color: #e9a0ff;
+  button {
+    background-color: #333;
+    color: white;
+    padding: 7px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    &:hover {
+      background-color: #494949;
+    }
+  }
+`;
+
+// const StyledButton = styled.button`
+//   background-color: black;
+//   color: white;
+// `;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledDiv>
+      Hello bitch, nice TITS ahahahahah milky millky milky baby thirsty mommy
+      baby want milk suck suck suck suck hahahaha stupid cunt give me those big
+      udders you slut hahahaha tits tit titty me your caveman me use big titty
+      for big bitty hahaha honk honk honk slut cunt mommy honk honk milky baby
+      want more now honk honk honk pitter patter on those big mommy milkies hee
+      hee hee haha haaaa haaaa can’t stop the milk truck coming through honk
+      honk all aboard the titty train hee hee woo woooooo honk honk honk!!!
+      {/* <StyledButton>Tits</StyledButton> */}
+      <button>tits</button>
+    </StyledDiv>
   );
 }
 
