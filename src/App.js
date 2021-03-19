bd241e30055a44a4b58fe7e087455375dca8258a
Sahir Patel <sahirmpatel@gmail.com>
2021-03-19 11:47:03 +0530

    search parameter component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,28 @@ import { Container } from "react-bootstrap";
 import { useState } from "react";
 import Job from "./components/Job";
 import JobsPagination from "./components/JobsPagination";
+import SearchForm from "./components/SearchForm";
 function App() {
   const [params, setParams] = useState({});
   const [page, setPage] = useState(1);
 
   const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
-  console.log("jobs:", jobs);
+
+  // function for updating params
+  function handleParamChange(e) {
+    // creating key value pair
+    const param = e.target.name;
+    const value = e.target.value;
+    // setting page back to one because its a new request
+    setPage(1);
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
+  }
 
   return (
     <Container>
+      <SearchForm params={params} onParamChange={handleParamChange} />
       <JobsPagination page={page} hasNextPage={hasNextPage} setPage={setPage} />
       {loading && <h1>loading...</h1>}
       {error && <h1>Error . Try refreshing.</h1>}
---
+++ src/components/SearchForm.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+export default function SearchForm({ params, onParamChange }) {
+  return (
+    <form action="">
+      <input
+        value={params.description}
+        name="description"
+        onChange={onParamChange}
+        placeholder="description"
+        type="text"
+      />
+      <input
+        value={params.location}
+        name="location"
+        onChange={onParamChange}
+        placeholder="location"
+        type="text"
+      />
+      <input
+        onChange={onParamChange}
+        value={params.full_time}
+        name="full_time"
+        id="full-time"
+        label="Only Full Time"
+        type="checkbox"
+      />
+    </form>
+  );
+}
