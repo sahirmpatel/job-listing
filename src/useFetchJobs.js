f936588b6181298fcfe808c2152003e3ea38657f
Sahir Patel <sahirmpatel@gmail.com>
2021-06-20 12:20:45 +0530

    switching to mockdata temporarily

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,6 +1,6 @@
 import { useReducer, useEffect } from "react";
 import ghapi from "./api";
-import axios from 'axios'
+import axios from "axios";
 
 // Use the mockjsondata whenever making ui changes - to prevent overuse of the api
 import mockjsondata from "./mockdata.json";
@@ -12,11 +12,9 @@ const ACTIONS = {
   UPDATE_HAS_NEXT_PAGE: "update-has-next-page",
 };
 
-
 // Adding additional methods to the axios instance
 ghapi.CancelToken = axios.CancelToken;
 ghapi.isCancel = axios.isCancel;
-
 
 function reducer(state, action) {
   switch (action.type) {
@@ -39,50 +37,50 @@ function reducer(state, action) {
 }
 
 export default function useFetchJobs(params, page) {
-  const initialState = { jobs: [], loading: true };
-  // const initialState = { jobs: mockjsondata, loading: true };
+  // const initialState = { jobs: [], loading: true };
+  const initialState = { jobs: mockjsondata, loading: false };
   // console.log("initialState:", initialState);
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  useEffect(() => {
-    
-    const cancelToken1 = ghapi.CancelToken.source();
-    dispatch({ type: ACTIONS.MAKE_REQUEST });
-    ghapi
-      .get('/', {
-        cancelToken: cancelToken1.token,
-        params: { markdown: true, page: page, ...params },
-      })
-      .then((res) => {
-        dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } });
-      })
-      .catch((e) => {
-        if (ghapi.isCancel(e)) return;
-        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
-      });
+  // useEffect(() => {
 
-    const cancelToken2 = ghapi.CancelToken.source();
-    ghapi
-      .get('/', {
-        params: { markdown: true, page: page + 1, ...params },
-        cancelToken: cancelToken2.token,
-      })
-      .then((res) => {
-        dispatch({
-          type: ACTIONS.UPDATE_HAS_NEXT_PAGE,
-          payload: { hasNextPage: res.data.length !== 0 },
-        });
-      })
-      .catch((e) => {
-        if (ghapi.isCancel(e)) return;
-        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
-      });
+  //   const cancelToken1 = ghapi.CancelToken.source();
+  //   dispatch({ type: ACTIONS.MAKE_REQUEST });
+  //   ghapi
+  //     .get('/', {
+  //       cancelToken: cancelToken1.token,
+  //       params: { markdown: true, page: page, ...params },
+  //     })
+  //     .then((res) => {
+  //       dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } });
+  //     })
+  //     .catch((e) => {
+  //       if (ghapi.isCancel(e)) return;
+  //       dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+  //     });
 
-    return () => {
-      cancelToken1.cancel();
-      cancelToken2.cancel();
-    };
-  }, [params, page]);
+  //   const cancelToken2 = ghapi.CancelToken.source();
+  //   ghapi
+  //     .get('/', {
+  //       params: { markdown: true, page: page + 1, ...params },
+  //       cancelToken: cancelToken2.token,
+  //     })
+  //     .then((res) => {
+  //       dispatch({
+  //         type: ACTIONS.UPDATE_HAS_NEXT_PAGE,
+  //         payload: { hasNextPage: res.data.length !== 0 },
+  //       });
+  //     })
+  //     .catch((e) => {
+  //       if (ghapi.isCancel(e)) return;
+  //       dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+  //     });
+
+  //   return () => {
+  //     cancelToken1.cancel();
+  //     cancelToken2.cancel();
+  //   };
+  // }, [params, page]);
 
   return state;
 }
